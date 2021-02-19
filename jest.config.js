bc10f829f9b5bfe6ba663847316742184691f119
Denis Alberto Santana Fevereiro <denis.alberto@totvs.com.br>
2021-02-18 23:08:14 -0300

    chore: config mongodb

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,17 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/main/**'
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!<rootDir>**/test/**'
   ],
   coverageDirectory: "coverage",
   coverageProvider: "babel",
   testEnvironment: "node",
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
-};
+}
