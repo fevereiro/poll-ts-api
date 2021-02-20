7b3f8bc1d9004276e8c69a42dd29aae6dc236881
Denis Alberto Santana Fevereiro <denis.fevereiro@gmail.com>
2021-02-20 11:03:54 -0300

    feat: ensure request return json as default

--- src/main/middlewares/cors.test.ts
+++ src/main/middlewares/cors.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 import app from '../config/app'
 
 describe('CORS Middleware', () => {
-    test('should enabler CORS', async () => {
+    test('should enable CORS', async () => {
         app.get('/test_cors', (req, res) => {
             res.send()
         })
