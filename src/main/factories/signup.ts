a2e9d926b95aa0f7b40d5881c46642d1e27490a6
Denis Alberto Santana Fevereiro <denis.fevereiro@gmail.com>
2021-02-21 21:45:24 -0300

    feat: create controller decorator

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,15 +1,17 @@
+import { LogControllerDecorator } from './../decorators/log';
 import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account';
 import { DbAddAccount } from './../../data/usecases/add-account/db-add-account';
 import { EmailValidatorAdapter } from './../../utils/email-validator';
 import { SignUpController } from './../../presentation/controllers/singup/singup';
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter';
+import { Controller } from '../../presentation/protocols';
 
-
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
     const salt = 12
     const emailValidatorAdapter = new EmailValidatorAdapter()
     const bcryptAdapter = new BcryptAdapter(salt)
     const accountMongoRepository = new AccountMongoRepository()
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-    return new SignUpController(emailValidatorAdapter, dbAddAccount)
+    const singUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+    return new LogControllerDecorator(singUpController)
 }
