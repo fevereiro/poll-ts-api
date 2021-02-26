7f81936c994806f9954d050d63d2061b1abf6d99
Denis Alberto Santana Fevereiro <denis.fevereiro@gmail.com>
2021-02-25 21:03:02 -0300

    feat: ensure ExpressRouterAdapter returns the error message

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,3 +1,4 @@
+import { LogMongoRepository } from './../../infra/db/mongodb/log-repository/log';
 import { LogControllerDecorator } from './../decorators/log';
 import { AccountMongoRepository } from './../../infra/db/mongodb/account-repository/account';
 import { DbAddAccount } from './../../data/usecases/add-account/db-add-account';
@@ -13,5 +14,6 @@ export const makeSignUpController = (): Controller => {
     const accountMongoRepository = new AccountMongoRepository()
     const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
     const singUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
-    return new LogControllerDecorator(singUpController)
+    const logMongoRepository = new LogMongoRepository()
+    return new LogControllerDecorator(singUpController, logMongoRepository)
 }
