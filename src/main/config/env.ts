60ff09bec36ed2daddc9fe10bee2b21fc5ba9cc1
Denis Alberto Santana Fevereiro <denis.fevereiro@gmail.com>
2021-05-31 21:36:07 -0300

    feat: add login router

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,5 @@
 export default {
     mongoUrl: process.env.MONGO_URL || 'mongodb://denis:e296cd9f@localhost:27017/',
-    port: process.env.PORT || 5050
+    port: process.env.PORT || 5050,
+    jwtSecret: process.env.JWT || 'tUz10F@'
 }
--- src/main/factories/login/login-factory.ts
+++ src/main/factories/login/login-factory.ts
@@ -1,18 +1,21 @@
-import { DbAddAccount } from "../../../data/usecases/add-account/db-add-account"
-import { BcryptAdapter } from "../../../infra/criptography/bcrypt-adapter/bcrypt-adapter"
-import { AccountMongoRepository } from "../../../infra/db/mongodb/account/account-mongo-repo"
-import { LogMongoRepository } from "../../../infra/db/mongodb/log/log-mongo-repo"
-import { SignUpController } from "../../../presentation/controllers/singup/singup-controller"
+import env from '../../config/env';
+import { JwtAdapter } from './../../../infra/criptography/jwt-adapter/jwt-adapter';
+import { makeLoginValidation } from './login-validation-factory';
+import { LogControllerDecorator } from './../../decorators/log-controller-decorator';
 import { Controller } from "../../../presentation/protocols"
-import { LogControllerDecorator } from "../../decorators/log-controller-decorator"
-import { makeSignUpValidation } from "../signup/signup-validation-factory"
+import { LogMongoRepository } from '../../../infra/db/mongodb/log/log-mongo-repo';
+import { DbAuthentication } from '../../../domain/usecases/authentication/db-authentication';
+import { LoginController } from '../../../presentation/controllers/login/login-controller';
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter';
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account/account-mongo-repo';
 
-export const makeSignUpController = (): Controller => {
+export const makeLoginController = (): Controller => {
     const salt = 12
     const bcryptAdapter = new BcryptAdapter(salt)
+    const jwtAdapter = new JwtAdapter(env.jwtSecret)
     const accountMongoRepository = new AccountMongoRepository()
-    const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
-    const singUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+    const dbAuthentication = new DbAuthentication(accountMongoRepository, bcryptAdapter, jwtAdapter, accountMongoRepository)
+    const loginController = new LoginController(dbAuthentication, makeLoginValidation())
     const logMongoRepository = new LogMongoRepository()
-    return new LogControllerDecorator(singUpController, logMongoRepository)
+    return new LogControllerDecorator(loginController, logMongoRepository)
 }
